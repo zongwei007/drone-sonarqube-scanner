42d241c4b9bf7c2ec4b3b24bd0cebc7ef5c95b45
Zongwei <zongwei007@gmail.com>
2019-12-11 02:18:17 +0800

    chore: 使用 hub 实现构建

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-docker build --build-arg SCANNER_VERSION=$SCANNER_VERSION . -t knives/drone-sonarqube-scanner
-
-curl -H "Content-Type: application/json" \
-  --data '{"source_type": "Branch", "source_name":"master"}' \
-  -X POST https://cloud.docker.com/api/build/v1/source/$DOCKER_USER_TOKEN/trigger/$DOCKER_HOOK_TOKEN/call/
+curl -X POST https://cloud.docker.com/api/build/v1/source/$DOCKER_USER_TOKEN/trigger/$DOCKER_HOOK_TOKEN/call/
